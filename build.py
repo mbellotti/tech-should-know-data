7b001d639c3f306d5510035a35099d82c05ab01b
mbellotti <marianne.bellotti@gmail.com>
2024-02-15 12:52:42 -0500

    fixing the build index script

--- build.py
+++ build.py
@@ -2,20 +2,25 @@ import os
 import json
 
 def count_tags(tags, article, name):
-    found = False
     for a in article["tags"]:
-        for t in tags:
-            if t["text"] == a:
-                t["value"] += 1
-                t["items"].append({"title":article["title"], "uri": name})
-                found = True
-        if not found:
-            tags.append({"text":a, "value":1, "items":[{"title":article["title"], "uri": name}]})
+        try:
+            tags[a]["value"] += 1
+            tags[a]["items"].append({"title":article["title"], "uri": name})
+        except KeyError:
+            tags[a] = {"text":a, "value":1, "items":[{"title":article["title"], "uri": name}]}
     return tags
+
+def unpack_tags(tags):
+    # dict to list
+    tlist = []
+    for t in tags:
+        tlist.append(tags[t])
+    return tlist
 
 
 template = open("index.temp")
 ret = json.loads(template.read())
+tags = {}
 
 for root, dirs, files in os.walk("articles", topdown=False):
     for name in files:
@@ -23,7 +28,8 @@ for root, dirs, files in os.walk("articles", topdown=False):
         uri = name.split(".") # Don't want the .json part
         data = json.load(f)
         ret["articles"].append({"title":data["title"], "uri": uri[0]})
-        ret["tags"] = count_tags(ret["tags"], data, name)
+        tags = count_tags(tags, data, name)
 
+ret["tags"] = unpack_tags(tags)
 out = open("index.json", "w")
 out.write(json.dumps(ret))
